d0614ff016323d1d2f8728750e45f6b41d313f0b
Kieran <Kieran_rocky@hotmail.com>
2016-11-24 10:12:57 +0000

    Tests for exam

--- northwindTest.sql
+++ northwindTest.sql
@@ -1,5 +1,29 @@
 use northwind;
 
+show tables;
+
+select COUNT(*) from customers;
+
+select COUNT(*) from employees;
+
+select COUNT(*) from employee_privileges;
+
+select COUNT(*) from orders_tax_status;
+
+select COUNT(*) from orders_status;
+
+select COUNT(*) from orders;
+
+select COUNT(*) from purchase_order_status;
+
+select COUNT(*) from suppliers;
+
+select COUNT(*) from purchase_orders;
+
+select COUNT(*) from inventory_transactions;
+
+select COUNT(*) from invoices;
+
 select * from employees where id = < 10;
 
 select COUNT(id)AS NumberOfEmployees from employees;
